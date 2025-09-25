37baca0463233dbc5810918a8d4e8905b31ad57b
Armando Tavera <taveramex@gmail.com>
2025-09-25 17:10:47 -0600

    twisted a little the cranks to test component creation

---
+++ src/components/MyCoolApp.tsx
@@ -0,0 +1,13 @@
+function MyCoolApp(){
+
+    return (
+        <>
+            <div>
+                <h2>Title for cool Shite</h2>
+                <p>generic paragraph</p>
+            </div>
+        </>
+    )
+}
+
+export default MyCoolApp;

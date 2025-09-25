6745da17e0d486a27dadbe5b33824333f5ff909d
Armando Tavera <taveramex@gmail.com>
2025-09-25 17:25:20 -0600

    awful way to put css styles to components in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import type { CSSProperties } from "react";
 import MyCoolApp from "./components/MyCoolApp"
 
 function App() {
 
+  let isActive = false;
+  const someStylos:CSSProperties = {
+    backgroundColor: 'red',
+    color: 'green',
+    marginTop:30,
+  }
+
   return (
     <>
+
       <h1>Test App Component</h1>
       <div>This is a Test App to create stuff and learn</div>
-      <MyCoolApp  />
+      {isActive ? <MyCoolApp /> : <span>not cool App</span>}
+
+      <p style={{
+        color: 'red',
+        backgroundColor: '#fafafa',
+        borderRadius: isActive ? 10 : 0,
+      }}>
+        this is awful to format style
+      </p>
+      <p style={someStylos}>
+        other aberration to see but a little less awful
+      </p>
+
+
     </>
   )
 }
